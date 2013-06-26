b6ac108e412fde657f0b16abf141a9c36ab83e96
LEESY0723 <leesy401@naver.com>
2013-06-26 21:32:33 +0900

    프로젝트 생성

---
+++ src/hs/mirim/BUMPActivity.java
@@ -0,0 +1,12 @@
+package hs.mirim;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BUMPActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
